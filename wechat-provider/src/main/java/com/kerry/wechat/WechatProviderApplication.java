9a1e20cb0204f2a951e88cbaf124005db0919052
wangshen <280232086@qq.com>
2017-06-19 16:42:20 +0800

    添加微信服务提供方模块

---
+++ wechat-provider/src/main/java/com/kerry/wechat/WechatProviderApplication.java
@@ -0,0 +1,12 @@
+package com.kerry.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatProviderApplication.class, args);
+	}
+}
